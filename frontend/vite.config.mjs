b4501c15dd94c869169cf8238c818727a188925b
Ade Kurniawan <57614206+adekurniawan22@users.noreply.github.com>
2024-08-20 23:18:10 +0700

    change config vite

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -5,7 +5,7 @@ import autoprefixer from 'autoprefixer'
 
 export default defineConfig(() => {
     return {
-        base: './',
+        base: '/web-joki/',
         build: {
             outDir: 'build',
         },
